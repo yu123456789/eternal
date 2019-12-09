20d4e06b37fab9647b5927a53148c8e85d7effa3
yuy <1418191865@qq.com>
2019-12-09 10:34:12 +0800

    上传到仓库

---
+++ src/main/java/com/eternal/lover/LoverApplication.java
@@ -0,0 +1,13 @@
+package com.eternal.lover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eternal/lover/LoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eternal.lover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
